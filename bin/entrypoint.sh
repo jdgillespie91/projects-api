baa5afd3d29cef2b958a62a52c274c1264864283
Jake Gillespie <jake.gillespie@onefinestay.com>
2017-01-20 22:08:16 +0000

    Before running the app, fetch the setttings file from S3

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,53 +5,57 @@ set -e
 set -x
 
 
-function dev {
-    # In development mode, we want to use the source code as the project 
-    # source. This enables hot reloading of the application.
-    export PROJECT_ENV=dev
+function setup_dev {
+    setup dev
     pip install -e .
+}
+
+
+function run_dev {
     gunicorn projects.app:app --bind 0.0.0.0:8000 --reload
 }
 
 
-function local {
-    export PROJECT_ENV=local
-    gunicorn projects.app:app --bind 0.0.0.0:8000
+function setup {
+    aws s3 cp s3://settings-$1/settings.sh /tmp/settings.sh
+    source /tmp/settings.sh
+    rm /tmp/settings.sh
 }
 
 
-function ci {
-    export PROJECT_ENV=ci
-    gunicorn projects.app:app --bind 0.0.0.0:8000
-}
-
-
-function qa {
-    export PROJECT_ENV=qa
-    gunicorn projects.app:app --bind 0.0.0.0:8000
-}
-
-
-function staging {
-    export PROJECT_ENV=staging
-    gunicorn projects.app:app --bind 0.0.0.0:8000
-}
-
-
-function prod {
-    export PROJECT_ENV=prod
+function run {
     gunicorn projects.app:app --bind 0.0.0.0:8000
 }
 
 
 function main {
     case "$1" in
-        dev ) dev ;;
-        local ) local ;;
-        ci ) ci ;;
-        qa ) qa ;;
-        staging ) staging ;;
-        prod ) prod ;;
+        dev )
+            # In development mode, we want to use the source code as the 
+            # project source. This enables hot reloading of the application.
+            setup_dev
+            run_dev
+            ;;
+        local )
+            setup $1
+            run
+            ;;
+        ci )
+            setup $1
+            run
+            ;;
+        qa )
+            setup $1
+            run
+            ;;
+        staging )
+            setup $1
+            run
+            ;;
+        prod )
+            setup $1
+            run
+            ;;
     esac
 }
 
