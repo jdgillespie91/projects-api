701fabee109fc742ec28e9d503b0fc7a9c41a3c9
Jake Gillespie <jake.gillespie@onefinestay.com>
2017-04-01 09:00:49 +0100

    Add test according to https://trello.com/c/91nb06fC

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -5,72 +5,31 @@ set -e
 set -x
 
 
-function setup_dev {
-    setup dev
+function setup {
     pip install -e .
-}
-
-
-function run_dev {
-    gunicorn projects.app:app --bind 0.0.0.0:8000 --reload
-}
-
-
-function setup_test {
-    setup test
     pip install pytest
 }
 
 
-function run_test {
-    pytest tests/
-}
-
-
-function setup {
-    aws s3 cp s3://settings-$1/settings.sh /tmp/settings.sh
-    source /tmp/settings.sh
-    rm /tmp/settings.sh
-}
-
-
 function run {
-    gunicorn projects.app:app --bind 0.0.0.0:8000
+    gunicorn projects.app:app --bind 0.0.0.0:8000 --reload
+}
+
+
+function unittest {
+    pytest tests
 }
 
 
 function main {
     case "$1" in
         dev )
-            # In development mode, we want to use the source code as the 
-            # project source. This enables hot reloading of the application.
-            setup_dev
-            run_dev
+            setup
+            run
             ;;
         test )
-            # In test mode, we don't run the app. We only invoke the tests.
-            setup_test
-            run_test
-            ;;
-        local )
-            setup $1
-            run
-            ;;
-        ci )
-            setup $1
-            run
-            ;;
-        qa )
-            setup $1
-            run
-            ;;
-        staging )
-            setup $1
-            run
-            ;;
-        prod )
-            setup $1
-            run
+            setup
+            unittest
             ;;
     esac
 }
